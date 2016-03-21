85c3507b655b20a12bd988ea882d88af41018f7e
Yoshihiro Tanaka <contact@cordea.jp>
2016-03-21 14:22:17 +0900

    added test case

--- app/src/androidTest/kotlin/jp/cordea/mackerelclient/utils/DateUtilsTest.kt
+++ app/src/androidTest/kotlin/jp/cordea/mackerelclient/utils/DateUtilsTest.kt
@@ -2,6 +2,7 @@ package jp.cordea.mackerelclient.utils
 
 import android.test.AndroidTestCase
 import org.junit.Test
+import java.util.*
 
 /**
  * Created by CORDEA on 2016/03/21.
@@ -10,10 +11,19 @@ class DateUtilsTest() : AndroidTestCase() {
 
     @Test
     public fun testStringFromEpoch() {
+        val oldTz = TimeZone.getDefault()
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tokyo"))
+        assertEquals("00:00:00", DateUtils.stringFromEpoch(1458486000))
+        assertEquals("23:59:59", DateUtils.stringFromEpoch(1458572399))
+        TimeZone.setDefault(oldTz)
     }
 
     @Test
     public fun testStringDateFromEpoch() {
-
+        val oldTz = TimeZone.getDefault()
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tokyo"))
+        assertEquals("Mar 21, 2016 12:00:00 AM", DateUtils.stringDateFromEpoch(1458486000))
+        assertEquals("Mar 21, 2016 11:59:59 PM", DateUtils.stringDateFromEpoch(1458572399))
+        TimeZone.setDefault(oldTz)
     }
 }
