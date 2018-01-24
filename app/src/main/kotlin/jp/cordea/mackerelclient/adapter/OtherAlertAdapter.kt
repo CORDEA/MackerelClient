bc8b1e764c52f29c71260f857d837de512f5d826
Yoshihiro Tanaka <contact@cordea.jp>
2018-01-24 09:15:51 +0900

    use databinding

--- app/src/main/kotlin/jp/cordea/mackerelclient/adapter/OtherAlertAdapter.kt
+++ app/src/main/kotlin/jp/cordea/mackerelclient/adapter/OtherAlertAdapter.kt
@@ -5,26 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.TextView
 import jp.cordea.mackerelclient.R
 import jp.cordea.mackerelclient.api.response.Alert
-import jp.cordea.mackerelclient.view.CharCircleView
+import jp.cordea.mackerelclient.databinding.ListItemOtherAlartBinding
 
-class OtherAlertAdapter(context: Context, val items: List<Alert>) : ArrayAdapter<Alert>(context, R.layout.list_item_other_alart) {
+class OtherAlertAdapter(
+        context: Context,
+        val items: List<Alert>
+) : ArrayAdapter<Alert>(
+        context,
+        R.layout.list_item_other_alart
+) {
 
-    override fun getItem(position: Int): Alert {
-        return items[position]
-    }
+    override fun getItem(position: Int): Alert =
+            items[position]
 
-    override fun getCount(): Int {
-        return items.size
-    }
+    override fun getCount(): Int =
+            items.size
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
         var view = convertView
         val viewHolder: ViewHolder
         if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.list_item_other_alart, parent, false)
+            view = LayoutInflater.from(context)
+                    .inflate(R.layout.list_item_other_alart, parent, false)
             viewHolder = ViewHolder(view)
             view.tag = viewHolder
         } else {
@@ -33,10 +37,10 @@ class OtherAlertAdapter(context: Context, val items: List<Alert>) : ArrayAdapter
 
         val item = getItem(position)
 
-        viewHolder.apply {
-            detailTextView.text = item.type + " / " + item.status
+        viewHolder.binding.run {
+            detailTextView.text = "${item.type} / ${item.status}"
             nameTextView.text = item.hostId
-            statusCircleView.char = item.status.first()
+            statusView.char = item.status.first()
         }
 
         return view
@@ -44,11 +48,6 @@ class OtherAlertAdapter(context: Context, val items: List<Alert>) : ArrayAdapter
 
     class ViewHolder(view: View) {
 
-        val detailTextView = view.findViewById(R.id.detail) as TextView
-
-        val nameTextView = view.findViewById(R.id.name) as TextView
-
-        val statusCircleView = view.findViewById(R.id.status) as CharCircleView
-
+        val binding: ListItemOtherAlartBinding = ListItemOtherAlartBinding.bind(view)
     }
 }
