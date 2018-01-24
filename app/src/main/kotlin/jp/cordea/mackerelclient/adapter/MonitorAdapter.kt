ad21473bedfd0151170262ae867c20596a4e6e66
Yoshihiro Tanaka <contact@cordea.jp>
2018-01-24 09:24:58 +0900

    divide view holder

--- app/src/main/kotlin/jp/cordea/mackerelclient/adapter/MonitorAdapter.kt
+++ app/src/main/kotlin/jp/cordea/mackerelclient/adapter/MonitorAdapter.kt
@@ -18,7 +18,7 @@ class MonitorAdapter(
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
         val context = fragment.context ?: return
-        (holder as? ViewHolder)?.apply {
+        (holder as? ViewHolder)?.run {
             items[position].second?.let { item ->
                 cell.setOnClickListener {
                     val intent = MonitorDetailActivity.createIntent(context, item)
@@ -30,15 +30,20 @@ class MonitorAdapter(
                 id.text = item.id
                 return
             }
+        }
+        (holder as? SectionViewHolder)?.run {
             title.text = items[position].first
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder? {
-        var view = LayoutInflater.from(fragment.context).inflate(R.layout.list_item_monitor, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
         if (viewType == 1) {
-            view = LayoutInflater.from(fragment.context).inflate(R.layout.list_item_monitor_section, parent, false)
+            val view = LayoutInflater.from(fragment.context)
+                    .inflate(R.layout.list_item_monitor_section, parent, false)
+            return SectionViewHolder(view)
         }
+        val view = LayoutInflater.from(fragment.context)
+                .inflate(R.layout.list_item_monitor, parent, false)
         return ViewHolder(view)
     }
 
@@ -51,10 +56,14 @@ class MonitorAdapter(
     }
 
     private class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val cell: View by bindView(R.id.cell)
 
-        val title: TextView by bindView(R.id.title)
+        val cell: View by bindView(R.id.cell)
         val name: TextView by bindView(R.id.name)
         val id: TextView by bindView(R.id.id)
+    }
+
+    private class SectionViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+        val title: TextView by bindView(R.id.title)
     }
 }
