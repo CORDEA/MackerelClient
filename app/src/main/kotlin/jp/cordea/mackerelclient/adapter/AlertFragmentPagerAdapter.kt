289517d80ff905e2cd208ecac7c0d6c6838517ed
Yoshihiro Tanaka <contact@cordea.jp>
2018-01-27 10:05:09 +0900

    reformat code

--- app/src/main/kotlin/jp/cordea/mackerelclient/adapter/AlertFragmentPagerAdapter.kt
+++ app/src/main/kotlin/jp/cordea/mackerelclient/adapter/AlertFragmentPagerAdapter.kt
@@ -8,25 +8,25 @@ import jp.cordea.mackerelclient.R
 import jp.cordea.mackerelclient.fragment.alert.CriticalAlertFragment
 import jp.cordea.mackerelclient.fragment.alert.OtherAlertFragment
 
-class AlertFragmentPagerAdapter(fm: FragmentManager, val context: Context) : FragmentPagerAdapter(fm) {
+class AlertFragmentPagerAdapter(
+        manager: FragmentManager,
+        val context: Context
+) : FragmentPagerAdapter(manager) {
 
-    override fun getCount(): Int {
-        return 2
-    }
+    override fun getCount(): Int =
+            2
 
-    override fun getItem(position: Int): Fragment? {
-        when (position) {
-            0 -> return CriticalAlertFragment.newInstance()
-            1 -> return OtherAlertFragment.newInstance()
-        }
-        return null
-    }
+    override fun getItem(position: Int): Fragment? =
+            when (position) {
+                0 -> CriticalAlertFragment.newInstance()
+                1 -> OtherAlertFragment.newInstance()
+                else -> null
+            }
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        when (position) {
-            0 -> return context.getString(R.string.in_progress_alert_title)
-            1 -> return context.getString(R.string.closed_alert_title)
-        }
-        return null
-    }
+    override fun getPageTitle(position: Int): CharSequence? =
+            when (position) {
+                0 -> context.getString(R.string.in_progress_alert_title)
+                1 -> context.getString(R.string.closed_alert_title)
+                else -> null
+            }
 }
